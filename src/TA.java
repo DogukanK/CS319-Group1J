7e82606b379e632defd06292dec16d89d99fd60f
Anıl Taşdan <34445446+ATasdan@users.noreply.github.com>
2021-04-10 17:58:43 +0300

    Add files via upload
    Simple classes without major implementation, login UI implemented.

---
+++ src/TA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Group1JProject;
+
+/**
+ *
+ * @author anilt
+ */
+public class TA extends User{
+    
+    public TA(int id, String email, String name) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+}
